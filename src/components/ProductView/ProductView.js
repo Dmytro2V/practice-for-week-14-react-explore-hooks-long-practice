c7b81703796b4c6d1715e6488d30e3d4bf5018b0
Dmytro2V <op2vin@gmail.com>
2023-03-28 01:58:44 +0300

    done with bonus

--- src/components/ProductView/ProductView.js
+++ src/components/ProductView/ProductView.js
@@ -2,15 +2,30 @@ import React from 'react';
 import ProductListItem from "../ProductListItem";
 import ProductDetails from "../ProductDetails";
 import './ProductView.css'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 function ProductView({ products }) {
 
   // TODO: Replace with state variable
-  const [sideOpen, setSideOpen] = useState(true);
+  const [sideOpen, setSideOpen] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState('')
+  useEffect(() => {
+    console.log('opening, sideOpen = ',localStorage.getItem('sideOpen'));
+    if (  localStorage.getItem('sideOpen') === 'true' ||localStorage.getItem('sideOpen') === null) setSideOpen(true)
+  }, []);
+ 
+  useEffect(() => {
+    console.log(' opening side');
+    if (selectedProduct) setSideOpen(true);
+  }, [selectedProduct]);
+  useEffect(() => {
+    console.log('deselect prods');
+    if (!sideOpen) setSelectedProduct('');
 
-
+    // saving state
+    localStorage.setItem('sideOpen', sideOpen ? 'true': 'false')
+  }, [sideOpen]);
+console.log('-----------')
   return (
     <div className="product-view">
       <div className="product-main-area">
